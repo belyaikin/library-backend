466943c22d3977ca5fd2de8f20de1e6c2fcacb28
belyaikin <dmitriybelyaikin@gmail.com>
2026-02-11 15:37:17 +0500

    Move specific routes up

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -15,14 +15,15 @@ import { getEpub } from "../controllers/epubController.js";
 
 const router = Router();
 
-router.get("/", getAllBooks);
-router.get("/:id", getBookById);
-router.post("/", authenticateAccessToken, upload.single("epub"), registerBook);
-router.put("/:id", authenticateAccessToken, upload.single("epub"), updateBook);
 router.put("/favorite/:id", authenticateAccessToken, addAsFavorite);
 router.delete("/favorite/:id", authenticateAccessToken, removeFromFavorites);
 router.put("/buy/:id", authenticateAccessToken, buyBook);
 router.get("/epub/:id", authenticateAccessToken, getEpub);
+
+router.get("/", getAllBooks);
+router.get("/:id", getBookById);
+router.post("/", authenticateAccessToken, upload.single("epub"), registerBook);
+router.put("/:id", authenticateAccessToken, upload.single("epub"), updateBook);
 router.delete("/:id", authenticateAccessToken, deleteBook);
 
 export default router;
