56e59f06fc08e778f62101c9132e70b2308ed6dd
belyaikin <dmitriybelyaikin@gmail.com>
2026-02-11 14:42:16 +0500

    add delete review route

--- src/routes/reviewRoutes.ts
+++ src/routes/reviewRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import {
   getReviewsByBookId,
   registerReview,
+  unregisterReview,
 } from "../controllers/reviewController.js";
 import authenticateAccessToken from "../middleware/authMiddleware.js";
 
@@ -9,5 +10,6 @@ const router = Router();
 
 router.get("/:bookId", getReviewsByBookId);
 router.post("/:bookId", authenticateAccessToken, registerReview);
+router.delete("/:id", authenticateAccessToken, unregisterReview)
 
 export default router;
